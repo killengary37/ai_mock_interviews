1123bdfc907fe0b0c8414643025d00ff11234cea
Gary Marc Killen <killengary37@gmail.com>
2025-04-04 02:10:05 -0700

    setup app, file and folder structure, routing and auth pages, firebase and VAPI

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,11 +1,8 @@
-import {generateText} from "ai";
-import {google} from "@ai-sdk/google";
-import {getRandomInterviewCover} from "@/lib/utils";
-import {db} from "@/firebase/admin";
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
 
-export async function GET() {
-    return Response.json({success: true, data: 'THANK YOU'}, {status: 200})
-}
+import { db } from "@/firebase/admin";
+import { getRandomInterviewCover } from "@/lib/utils";
 
 export async function POST(request: Request) {
     const { type, role, level, techstack, amount, userid } = await request.json();
@@ -26,25 +23,29 @@ export async function POST(request: Request) {
         
         Thank you! <3
     `,
-
         });
 
         const interview = {
-            role, type, level, techstack: techstack.split(','),
+            role: role,
+            type: type,
+            level: level,
+            techstack: techstack.split(","),
             questions: JSON.parse(questions),
             userId: userid,
             finalized: true,
             coverImage: getRandomInterviewCover(),
-            createdAt: new Date().toISOString()
-        }
+            createdAt: new Date().toISOString(),
+        };
 
         await db.collection("interviews").add(interview);
 
-        return Response.json({success: true})
-
+        return Response.json({ success: true }, { status: 200 });
     } catch (error) {
-        console.error(error);
-
-        return Response.json({success: true}, {status: 200});
+        console.error("Error:", error);
+        return Response.json({ success: false, error: error }, { status: 500 });
     }
+}
+
+export async function GET() {
+    return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
 }
